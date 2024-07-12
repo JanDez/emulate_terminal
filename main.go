0f3d6aa20d0436f08f30d3619e637312cef71442
Jaifran Hernandez <jaifranjose@gmail.com>
2024-07-12 01:33:07 -0400

    Feat: Initial commit, initial attempt to emulate a simple cmd with go

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Simple Terminal")
+	fmt.Println("---------------------")
+
+	for {
+		fmt.Print("> ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		if input == "exit" {
+			break
+		}
+		fmt.Println("You entered:", input)
+	}
+}
